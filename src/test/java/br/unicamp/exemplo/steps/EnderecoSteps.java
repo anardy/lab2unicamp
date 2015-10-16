f1f3955c2d578ad9b9ed83f3a2aa773e19fecd95
Grupo03 - Andre Nardy <macknardy@gmail.com>
2015-10-16 20:47:51 -0300

    acento

--- src/test/java/br/unicamp/exemplo/steps/EnderecoSteps.java
+++ src/test/java/br/unicamp/exemplo/steps/EnderecoSteps.java
@@ -37,7 +37,7 @@ public class EnderecoSteps {
     	}
     }
     
-    @Dado("^O CEP do usuario nao é conhecido pelo sistema$")
+    @Dado("^O CEP do usuario nao e conhecido pelo sistema$")
     public void o_CEP_do_usuario_nao_e_conhecido_pelo_sistema(String arg1) throws Throwable {
     	try{
     	stubFor(get(WireMock.urlMatching("/viacep/ws/.*/json/"))
